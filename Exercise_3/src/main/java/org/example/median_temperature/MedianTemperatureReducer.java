16340c428c3b99db809ad7f862fc9b531d87e53f
Der eloquente Elias <eso7445@students.fhv.at>
2025-12-16 16:21:37 +0100

    reducer

--- Exercise_3/src/main/java/org/example/median_temperature/MedianTemperatureReducer.java
+++ Exercise_3/src/main/java/org/example/median_temperature/MedianTemperatureReducer.java
@@ -1,12 +1,18 @@
 package org.example.median_temperature;
 
-
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.DoubleWritable;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.mapred.AvroKey;
+import org.apache.avro.mapred.AvroValue;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
 
-public class MedianTemperatureReducer extends Reducer<Text, IntWritable, Text, DoubleWritable> {
+public class MedianTemperatureReducer extends Reducer<AvroKey<Integer>, AvroValue<GenericRecord>, AvroKey<GenericRecord>, NullWritable> {
+    @Override
+    protected void reduce(AvroKey<Integer> key, Iterable<AvroValue<GenericRecord>> values, Context context) throws IOException, InterruptedException {
 
+    }
 }
+
+
